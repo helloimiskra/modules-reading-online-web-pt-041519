df46b109db6766c36b83d5a10db69c2e46fb3073
Iskra B <ibatistapoblete@gmail.com>
2019-05-27 07:58:27 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,11 +3,9 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  extend
-  FancyDance::ClassMethods
-  include
-  FancyDance::InstanceMethods
-
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  
 
   attr_accessor :name
 
